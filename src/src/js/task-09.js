6e316e21c75ceacd77dbefc551768950c65b21ec
Sergiy-dp <sergeyeremenko16@gmail.com>
2023-04-17 22:52:28 +0300

    Update task-09.js

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -1,3 +1,19 @@
+const changeColorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+})
+
+
+
+
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
