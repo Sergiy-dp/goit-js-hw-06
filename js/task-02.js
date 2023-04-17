a3750f4ef4771142f9f8e3ee005d58add0047960
Sergiy-dp <sergeyeremenko16@gmail.com>
2023-04-17 23:01:34 +0300

    Create task-02.js

---
+++ js/task-02.js
@@ -0,0 +1,21 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+// 1. отримав посилання на елемент списку ul
+const ul = document.querySelector("#ingredients");
+// 2. Використовую forEach для перебору масиву і створення нового ел-та li
+ingredients.forEach((ingredient) => {
+  // 3. Створюю новий ел=т за допомогою методу createElement.
+  const li = document.createElement("li");
+  // 4. Додаю текстовий вміст з назвою інгредієнта.
+  li.textContent = ingredient;
+  // 5. додаю ел-ту клас item.
+  li.classList.add("item");
+// 5. помстив ul на сторінку
+  ul.appendChild(li);
+});
